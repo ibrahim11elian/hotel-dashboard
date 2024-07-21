e9b16bd1b7606086c2eddc97475cac127686c05e
ibrahim11elian <ibrahim11elian@gmail.com>
2024-07-21 16:51:37 +0300

    fix tailwind class color not working on stat card issue

--- src/features/dashboard/Stat.jsx
+++ src/features/dashboard/Stat.jsx
@@ -1,10 +1,16 @@
 function Stat({ icon, title, value, color }) {
+  const bgColor = `var(--color-${color}-100)`;
+  const textColor = `var(--color-${color}-700)`;
+
   return (
     <div className="grid grid-cols-[4rem_1fr] grid-rows-[auto_auto] gap-x-4 gap-y-1 rounded-md border border-gray-100 bg-white p-4">
       <div
-        className={`row-span-full flex aspect-square items-center justify-center rounded-full bg-${color}-100`}
+        className="row-span-full flex aspect-square items-center justify-center rounded-full"
+        style={{ backgroundColor: bgColor }}
       >
-        <div className={`text-3xl text-${color}-700`}>{icon}</div>
+        <div className="text-3xl" style={{ color: textColor }}>
+          {icon}
+        </div>
       </div>
       <h5 className="self-end text-xs font-semibold uppercase tracking-wide text-gray-500">
         {title}
